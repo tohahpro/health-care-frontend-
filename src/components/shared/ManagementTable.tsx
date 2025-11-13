32923197f6ad88309550a2e54ec093fefc68ae7a
Toha Hossain <tohahpro@gmail.com>
2025-11-13 11:00:21 +0600

    Creating Reusable Management Page Header and Refresh Button Components

--- src/components/shared/ManagementTable.tsx
+++ src/components/shared/ManagementTable.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Edit, Eye, Loader2, MoreHorizontal, Trash } from "lucide-react";
 import React from "react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
