d559ae3b949ced514a8a5036e9a8f9a2e161a70b
Toha Hossain <tohahpro@gmail.com>
2026-01-01 12:17:30 +0600

    proxy file updated

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,101 +1,84 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import jwt, { JwtPayload } from 'jsonwebtoken'
-import { getDefaultDashboardRoute, getRouteOwner, isAuthRoute, UserRole } from './lib/authUtils';
-import { deleteCookie, getCookie } from './services/auth/tokenHandlers';
-import { getUserInfo } from './services/auth/getUserInfo';
-
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import {
+  getDefaultDashboardRoute,
+  getRouteOwner,
+  isAuthRoute,
+  UserRole,
+} from "./lib/authUtils";
+import { cookies } from "next/headers";
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
+  const cookieStore = await cookies();
+  const pathname = request.nextUrl.pathname;
 
-    // const cookieStore = await cookies();
-    const pathname = request.nextUrl.pathname;
+  const accessToken = request.cookies.get("accessToken")?.value || null;
 
-    // const accessToken = request.cookies.get("accessToken")?.value || null;
+  let userRole: UserRole | null = null;
+  if (accessToken) {
+    const verifiedToken: string | JwtPayload = jwt.verify(
+      accessToken,
+      process.env.JWT_ACCESS_SECRET as string
+    );
 
-    const accessToken = await getCookie("accessToken") || null;
-
-
-    let userRole: UserRole | null = null;
-    if (accessToken) {
-        const verifiedToken: JwtPayload | string = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET as string)
-
-        if (typeof verifiedToken === "string") { // string return when token return error
-            // cookieStore.delete("accessToken")
-            // cookieStore.delete("refreshToken")
-            deleteCookie("accessToken")
-            deleteCookie("refreshToken")
-            return NextResponse.redirect(new URL('/login', request.url));
-        }
-
-        userRole = verifiedToken.role
+    if (typeof verifiedToken === "string") {
+      cookieStore.delete("accessToken");
+      cookieStore.delete("refreshToken");
+      return NextResponse.redirect(new URL("/login", request.url));
     }
 
-    const routeOwner = getRouteOwner(pathname)
+    userRole = verifiedToken.role;
+  }
 
-    const isAuth = isAuthRoute(pathname);
+  const routeOwner = getRouteOwner(pathname);
 
-    // Rule 1 : User is logged in and trying to access auth route
-    if (accessToken && isAuth) {
-        return NextResponse.redirect(new URL(getDefaultDashboardRoute(userRole as UserRole), request.url))
+  const isAuth = isAuthRoute(pathname);
+
+  if (accessToken && isAuth) {
+    return NextResponse.redirect(
+      new URL(getDefaultDashboardRoute(userRole as UserRole), request.url)
+    );
+  }
+
+  // Rule 2 : User is trying to access open public route
+  if (routeOwner == null) {
+    return NextResponse.next();
+  }
+
+  // Rule 1 & 2: Open public routes and auth routes
+  if (!accessToken) {
+    const loginUrl = new URL('/login', request.url);
+    loginUrl.searchParams.set('redirect', pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  // Rule 3: User is trying to access common protected route
+  if (routeOwner === "Common") {
+    return NextResponse.next();
+  }
+
+  // Rule 4: User is trying to access protected route
+  if(routeOwner === 'Admin' || routeOwner === 'Doctor' || routeOwner === 'Patient'){
+    if(userRole === routeOwner){
+        new URL(getDefaultDashboardRoute(userRole as UserRole), request.url);
     }
+    return NextResponse.next();
+  }
 
-    // Rule 2 : User is trying to access public route
-    if (routeOwner === null) {
-        return NextResponse.next()
-    }
-
-
-    if (!accessToken) {
-        const loginUrl = new URL("/login", request.url);
-        loginUrl.searchParams.set("redirect", pathname)
-        return NextResponse.redirect(loginUrl)
-    }
-
-    // Rule 3 : User need password change
-      if (accessToken) {
-        // const userInfo = await getUserInfo();
-        
-        // if (userInfo.needPasswordChange) {
-        //     if (pathname !== "/reset-password") {
-        //         const resetPasswordUrl = new URL("/reset-password", request.url);
-        //         resetPasswordUrl.searchParams.set("redirect", pathname);
-        //         return NextResponse.redirect(resetPasswordUrl);
-        //     }
-        //     return NextResponse.next();
-        // }
-
-        // if (userInfo && !userInfo.needPasswordChange && pathname === '/reset-password') {
-        //     return NextResponse.redirect(new URL(getDefaultDashboardRoute(userRole as UserRole), request.url));
-        // }
-    }
-
-    // Rule 4 : User is trying to access common protected route
-    if (routeOwner === 'Common') {
-        return NextResponse.next()
-    }
-
-    // Rule 5 : User is trying to access role base protected route
-    if (routeOwner === 'Admin' || routeOwner === 'Doctor' || routeOwner === 'Patient') {
-        if (userRole !== routeOwner) {
-            return NextResponse.redirect(new URL(getDefaultDashboardRoute(userRole as UserRole), request.url))
-        }
-        return NextResponse.next()
-    }
-
-    return NextResponse.next()
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - api (API routes)
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-         */
-        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.well-known).*)',
-    ],
-}
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.well-known).*)",
+  ],
+};
