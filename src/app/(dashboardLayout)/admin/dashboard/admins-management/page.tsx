d3480d2e5a98c94dcddeaf8dc4c556a8645c3921
Toha Hossain <tohahpro@gmail.com>
2026-01-08 22:05:59 +0600

    Admin management header, AdminFormDialog and adminsManagement server fetch setup

---
+++ src/app/(dashboardLayout)/admin/dashboard/admins-management/page.tsx
@@ -0,0 +1,13 @@
+import AdminsManagementHeader from "@/components/modules/Admin/AdminManagement/AdminManagementHeader";
+
+
+const AdminAdminsManagementPage = async () => {
+
+    return (
+        <div className="space-y-6">
+            <AdminsManagementHeader />
+        </div>
+    );
+};
+
+export default AdminAdminsManagementPage;
