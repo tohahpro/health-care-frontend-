a896686af0076475d1c0140fd4cb9dc351a96f4e
Toha Hossain <tohahpro@gmail.com>
2026-01-08 22:49:53 +0600

    admin management table and column setup

--- src/app/(dashboardLayout)/admin/dashboard/admins-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/admins-management/page.tsx
@@ -1,11 +1,35 @@
 import AdminsManagementHeader from "@/components/modules/Admin/AdminManagement/AdminManagementHeader";
+import AdminsTable from "@/components/modules/Admin/AdminManagement/AdminsTable";
+import TablePagination from "@/components/shared/TablePagination";
+import { TableSkeleton } from "@/components/shared/TableSkeleton";
+import { queryStringFormatter } from "@/lib/formatters";
+import { getAdmins } from "@/services/admin/adminsManagement";
+import { Suspense } from "react";
 
+const AdminAdminsManagementPage = async ({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
+    const searchParamsObj = await searchParams;
+    const queryString = queryStringFormatter(searchParamsObj);
+    const adminsResult = await getAdmins(queryString);
 
-const AdminAdminsManagementPage = async () => {
+    const totalPages = Math.ceil(
+        (adminsResult?.meta?.total || 1) / (adminsResult?.meta?.limit || 1)
+    );
 
     return (
         <div className="space-y-6">
             <AdminsManagementHeader />
+
+            <Suspense fallback={<TableSkeleton columns={8} rows={10} />}>
+                <AdminsTable admins={adminsResult?.data || []} />
+                <TablePagination
+                    currentPage={adminsResult?.meta?.page || 1}
+                    totalPages={totalPages || 1}
+                />
+            </Suspense>
         </div>
     );
 };
