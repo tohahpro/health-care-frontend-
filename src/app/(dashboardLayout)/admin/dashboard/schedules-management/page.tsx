4770139388c6131446beb71d212f94bf6a27dcb1
Toha Hossain <tohahpro@gmail.com>
2026-01-09 11:58:20 +0600

    schedule management table and columns setup with delete confirmation

--- src/app/(dashboardLayout)/admin/dashboard/schedules-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/schedules-management/page.tsx
@@ -1,11 +1,39 @@
 import SchedulesManagementHeader from "@/components/modules/Admin/SchedulesManagement/SchedulesManagementHeader";
+import SchedulesTable from "@/components/modules/Admin/SchedulesManagement/SchedulesTable";
+import TablePagination from "@/components/shared/TablePagination";
+import { TableSkeleton } from "@/components/shared/TableSkeleton";
+import { queryStringFormatter } from "@/lib/formatters";
+import { getSchedules } from "@/services/admin/schedulesManagement";
+import { Suspense } from "react";
 
 
-const SchedulesManagementPage = () => {
+const SchedulesManagementPage = async ({
+    searchParams }: {
+        searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+    }) => {
+
+    const searchParamsObj = await searchParams;
+
+    const queryString = queryStringFormatter(searchParamsObj);
+    const schedulesResult = await getSchedules(queryString);
+console.log(schedulesResult)
+    const totalPages = Math.ceil(
+        (schedulesResult?.meta?.total || 1) / (schedulesResult?.meta?.limit || 1)
+    );
 
     return (
         <>
-            <div><SchedulesManagementHeader /></div>
+            <div className="space-y-6">
+                <SchedulesManagementHeader />
+
+                <Suspense fallback={<TableSkeleton columns={4} rows={10} />}>
+                    <SchedulesTable schedules={schedulesResult?.data || []} />
+                    <TablePagination
+                        currentPage={schedulesResult?.meta?.page || 1}
+                        totalPages={totalPages || 1}
+                    />
+                </Suspense>
+            </div>
         </>
     );
 };
